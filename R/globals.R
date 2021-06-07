5b442263a24bf4e9b5464fcedd56cd7e22c94778
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2021-06-07 18:52:07 -0400

    Maintainer information update
    1. The email of the package maintainer is changed.
    2. Include '..by' to global variables.

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 # definition of global variables to solve output note of R CMD check
 
-utils::globalVariables(c("..fw", "..vars", "Pg", "Pn", "Png", "Png_Pn_Pg", "Png_log", "fw", "p", "p.x", "p.y", "sum_d"))
+utils::globalVariables(c("..fw", "..vars", "Pg", "Pn", "Png", "Png_Pn_Pg", "Png_log", "fw", "p", "p.x", "p.y", "sum_d", "..by"))
