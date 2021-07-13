2dfd589d449c00e99eb6b54733434e818ac40f3a
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2021-07-12 21:00:20 -0400

    A test database is included for examples of the 'mutual' function.

--- R/Data_Source.R
+++ R/Data_Source.R
@@ -56,3 +56,26 @@
 #'
 #' Education Quality Agency: \url{https://www.agenciaeducacion.cl/}
 "DT_Seg_Chile"
+
+#' @title Segregation data in southern Chile
+#' @description The data set included in this package was build using two data sets. The first one is the student
+#' enrollment reported by the Ministry of Education (MINEDUC, \url{https://datosabiertos.mineduc.cl/}) for students of primary
+#' education (first eight years of formal education) who attended establishments officially recognized by the State.
+#' The second one is the Quality and Context of Education Questionnaire for Parents and Guardians, and the Student
+#' Questionnaire, both applied by the Education Quality Agency (\url{https://www.agenciaeducacion.cl/}) to all students in
+#' grades 4 and 8 of primary education. Both sources are limited to 2018. Contains information related
+#' to students and educational system characteristics in southern Chile (Biobio, La Araucania and Los Rios regions).
+#' @format A \code{data.table} with 6703 observations and 5 variables, only for testing pourposes:
+#' \describe{
+#' \item{school}{School ID (RBD, Rol de Base de Datos).}
+#' \item{csep}{Socio-economic criterion. Students belong to either the non-vulnerable, the preferent, or the prioritized
+#' group acording to the Act 20.248 of Preferencial Scholar Subsidy (SEP).}
+#' \item{ethnicity}{Ethnicity criterion. Students belong to mapuche ethnicity or not.}
+#' \item{region}{Official region code of the school location. Schools can belong either Biobio region, La Araucania
+#' region or Los Rios region.}
+#' \item{fw}{Number of students in a cell or combination of variables.}
+#' }
+#' @source Ministry of Education (MINEDUC): \url{https://datosabiertos.mineduc.cl/}
+#'
+#' Education Quality Agency: \url{https://www.agenciaeducacion.cl/}
+"DT_test"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,3 +5,9 @@ usethis::use_data(DF_Seg_Chile, overwrite = TRUE)
 
 DT_Seg_Chile <- prepare_data(data = DF_Seg_Chile, vars = "all_vars", fw = "nobs", col.order = "region")
 usethis::use_data(DT_Seg_Chile, overwrite = TRUE)
+
+DT_test <- prepare_data(data = DF_Seg_Chile, vars = c("school", "csep", "ethnicity", "region", "year", "grade"), fw = "nobs",
+                        col.order = "region")
+DT_test <- DT_test[year == 2018 & grade == 4, ]
+DT_test <- DT_test[, -c("year", "grade")]
+usethis::use_data(DT_test, overwrite = TRUE)
